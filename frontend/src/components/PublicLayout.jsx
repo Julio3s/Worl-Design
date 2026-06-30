598951841ffccf8e78b3de5a1484f3fb39b4c8b7
farouk <faroukmissi7@gmail.com>
2026-06-30 20:39:08 +0100

    Correction: footer masqué pendant les transitions de page (plus de footer visible entre deux pages)

--- frontend/src/components/PublicLayout.jsx
+++ frontend/src/components/PublicLayout.jsx
@@ -1,4 +1,5 @@
-import { Outlet } from 'react-router-dom';
+kboimport { useEffect, useRef, useState } from 'react';
+import { Outlet, useLocation } from 'react-router-dom';
 
 import { Footer } from './Footer';
 import { Navbar } from './Navbar';
@@ -6,8 +7,27 @@ import { PageTransition } from './PageTransition';
 import { ScrollToTop } from './ScrollToTop';
 
 export function PublicLayout() {
+  const location = useLocation();
+  const [showFooter, setShowFooter] = useState(true);
+  const prevPathname = useRef(location.pathname);
+
+  useEffect(() => {
+    if (prevPathname.current !== location.pathname) {
+      prevPathname.current = location.pathname;
+      setShowFooter(false);
+      window.scrollTo(0, 0);
+    }
+  }, [location.pathname]);
+
+  useEffect(() => {
+    if (!showFooter) {
+      const id = requestAnimationFrame(() => setShowFooter(true));
+      return () => cancelAnimationFrame(id);
+    }
+  });
+
   return (
-    <div 
+    <div
       className="flex min-h-screen flex-col bg-cream text-text-dark"
       style={{
         width: "100%",
@@ -23,7 +43,7 @@ export function PublicLayout() {
           <Outlet />
         </PageTransition>
       </main>
-      <Footer />
+      {showFooter ? <Footer /> : null}
     </div>
   );
 }
